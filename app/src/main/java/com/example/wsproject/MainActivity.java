7d71148bc2abe96727209645764f12491776e573
Android User <Android User@nb-omen-01.sttec.net>
2019-11-19 15:06:01 +0300

    save

---
+++ app/src/main/java/com/example/wsproject/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.wsproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3*1000);
+    }
+}
